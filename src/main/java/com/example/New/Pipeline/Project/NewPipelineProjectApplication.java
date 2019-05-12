f9471310ee8578cc91fe8d9a77ad4da6dacc07db
Abhijit <abhijit.vz@gmail.com>
2019-05-12 16:50:59 +0530

    new proj

---
+++ src/main/java/com/example/New/Pipeline/Project/NewPipelineProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.New.Pipeline.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewPipelineProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewPipelineProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/New/Pipeline/Project/NewPipelineProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.New.Pipeline.Project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NewPipelineProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
